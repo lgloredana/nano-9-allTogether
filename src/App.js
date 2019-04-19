ea54894e5e7c8466c0aeba9780b6182726b6da0e
Loredana Lungu <loredana.lungu@tradeshift.com>
2019-04-19 13:58:34 +0300

    add npm delight survey

--- src/App.js
+++ src/App.js
@@ -23,37 +23,21 @@ class App extends Component {
    ));
  }
 componentWillMount() {
-    !function (e, t, r, n, a) {
-        if (!e[a]) {
-            for (var i = e[a] = [], c = 0; c < r.length; c++) {
-                var s = r[c];
-                i[s] = i[s] || function (e) {
-                    return function () {
-                        var t = Array.prototype.slice.call(arguments);
-                        i.push([e, t])
-                    }
-                }(s)
-            }
-            i.SNIPPET_VERSION = "1.0.1";
-            var o = t.createElement("script");
-            o.type = "text/javascript", o.async = !0, o.src = "https://d2yyd1h5u9mauk.cloudfront.net/integrations/web/v1/library/" + n + "/" + a + ".js";
-            var p = t.getElementsByTagName("script")[0];
-            p.parentNode.insertBefore(o, p)
-        }
-    }(window, document, ["survey", "reset", "config", "init", "set", "get", "event", "identify", "track", "page", "screen", "group", "alias"],
-        "joMcbxC55nAvcSY0", "delighted");
+    !function(e,t,r,n,a){if(!e[a]){for(var i=e[a]=[],c=0;c<r.length;c++){var s=r[c];i[s]=i[s]||function(e){return function(){var t=Array.prototype.slice.call(arguments);i.push([e,t])}}(s)}i.SNIPPET_VERSION="1.0.1";var o=t.createElement("script");o.type="text/javascript",o.async=!0,o.src="https://d2yyd1h5u9mauk.cloudfront.net/integrations/web/v1/library/"+n+"/"+a+".js";var p=t.getElementsByTagName("script")[0];p.parentNode.insertBefore(o,p)}}(window,document,["survey","reset","config","init","set","get","event","identify","track","page","screen","group","alias"],"joMcbxC55nAvcSY0","delighted");
 }
 
     render() {
+        window.delighted.survey({
+            email: "1010101lo@delighted.com",
+            name: "Bailey Dixon",               // customer name (optional)
+            minSurveyInterval: 0,
+            minTimeOnPage: 30,
+            properties: {                       // extra context (optional)
+                orderId: "123ABC",
+                shippingMethod: "Ground"
+            }
+        });;
 
-     window.delighted.survey({
-          email: "eliaearth24@gmail.com", // customer email (optional)
-          name: "Loredana Mihalce",               // customer name (optional)
-          properties: {                       // extra context (optional)
-              orderId: "order111111",
-              shippingMethod: "shiping1Itemt by internet"
-          }
-      });
     const {users, messages} = this.state;
     return (
       <div className="App">
